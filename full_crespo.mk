d75146524d6aee30eca7601258fd8ee3bb14e730
Bamtau <bamtaumod@gmail.com>
2012-01-23 02:16:54 -0500

    Updated

--- full_crespo.mk
+++ full_crespo.mk
@@ -25,6 +25,11 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Camera
 PRODUCT_PACKAGES := \
     Camera
+    Stk \
+    FileManager \
+    Trebuchet \
+    CMSettings \
+    LiberySettings \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
